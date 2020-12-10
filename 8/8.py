4241e0632bffb6c33b8ba92f5bc7730505aea91d
knoppanthony <knopp.anthony@gmail.com>
2020-12-09 21:04:30 -0500

    day 8 part 2, not bad. hard to read

--- 8/8.py
+++ 8/8.py
@@ -8,30 +8,49 @@ def main():
     partOne(instruction_list)
     partTwo(instruction_list)
 
-
+#Brute force...change every instruction...see if we loop.
 def partTwo(instruction_list):
     accumulator = 0
     i = 0 
-    seenInstructions = []
-    while i < len(instruction_list):
-        ins,amount = instruction_list[i].strip().split(" ")
+    modInstructionIndex=  0
 
-        if i in seenInstructions:
-            print(accumulator)
-            break
+    
+    while modInstructionIndex < len(instruction_list):
+        print(modInstructionIndex)
+        accumulator = 0
+        i = 0
+        seenInstructions = []
+        while i < len(instruction_list):
+            ins,amount = instruction_list[i].strip().split(" ")
 
-        seenInstructions.append(i)
-        
-        if ins == "acc":
-            accumulator += int(amount)
-            i += 1
-        elif ins == "jmp":
-            i += + int(amount)
-        elif ins == "nop":
-            i += 1
-        else:
-            print("EOF or we did something wrong")
-        print(ins + " " + amount + " " + str(i))
+            if i in seenInstructions:
+                print("repeat at accum:" + str(accumulator))
+                modInstructionIndex += 1
+                break
+
+            seenInstructions.append(i)
+
+            if modInstructionIndex == i:
+                if ins == "jmp":
+                    ins = "nop"
+                elif ins == "nop":
+                    ins = "jmp"
+                else:
+                    ins = ins
+
+            if ins == "acc":
+                accumulator += int(amount)
+                i += 1
+            elif ins == "jmp":
+                i += + int(amount)
+            elif ins == "nop":
+                i += 1
+            else:
+                print("EOF or we did something wrong")
+
+            if i == len(instruction_list) - 1 and ins != "jmp":
+                print("AccumWeDidIt:" + str(accumulator))
+
 
 
 def partOne(instruction_list):
